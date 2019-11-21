1830fcc8759d118f932cb239e0dd85d4313a4e4c
Nick <ngonzo95@gmail.com>
2019-11-20 22:00:53 -0600

    Removed unneeded model

--- app/main/response/run_map_response.py
+++ app/main/response/run_map_response.py
@@ -4,11 +4,6 @@ from app.main.controller.rest_plus_api import Api
 
 class RunMapResponse:
     api = Api.api
-    marker = api.model('marker', {
-        'mapId': fields.String(required=True, description='map id'),
-        'text': fields.String(required=True, description='text to display to the user'),
-        'cord': fields.List(fields.String, description='[lat, lon]')
-    })
 
     runMap = api.model('run_map', {
         "id": fields.String(description='The id of run map, only unique per user'),
