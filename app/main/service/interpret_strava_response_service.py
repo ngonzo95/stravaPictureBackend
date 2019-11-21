f299931f1e9b4527c277c2741e97d2ddbd990dce
Nick <ngonzo95@gmail.com>
2019-11-20 23:31:38 -0600

    added a method for parsing detailed activities

--- app/main/service/interpret_strava_response_service.py
+++ app/main/service/interpret_strava_response_service.py
@@ -1,3 +1,6 @@
+from app.main.model.run import Run
+
+
 def extract_ids_of_interest_from_activity_list(resJson):
     ids = []
     for activity in resJson:
@@ -5,3 +8,13 @@ def extract_ids_of_interest_from_activity_list(resJson):
             ids.append(activity['id'])
 
     return ids
+
+
+def create_run_from_detailed_activity(userId, activity):
+    dictOfInterest = {'id': activity['id'], 'userId': userId,
+                      'polyline': activity['map']['polyline'],
+                      'start': activity['start_latlng'],
+                      'name': activity['name'],
+                      'type': activity['type']
+                      }
+    return Run(dictOfInterest)
--- app/tests/unit/service/update_user_service_test.py
+++ app/tests/unit/service/update_user_service_test.py
@@ -1,5 +1,6 @@
 import app.tests.helpers.util.random_utils as random_utils
 import app.main.service.interpret_strava_response_service as unit
+from app.main.model.run import Run
 
 
 def test_collect_ids_of_interest():
@@ -12,6 +13,22 @@ def test_collect_ids_of_interest():
     idsOfInterest = unit.extract_ids_of_interest_from_activity_list(resJson)
     assert sorted([resJson[1]['id'], resJson[4]['id']]) \
         == sorted(idsOfInterest)
+
+
+def test_create_run_from_detailed_activity():
+    userId = random_utils.randomString(5)
+    activity = generate_detailed_activity()
+
+    run = unit.create_run_from_detailed_activity(userId, activity)
+
+    dictOfInterest = {'id': activity['id'], 'userId': userId,
+                      'polyline': activity['map']['polyline'],
+                      'start': activity['start_latlng'],
+                      'name': activity['name'],
+                      'type': activity['type']
+                      }
+    expectedRun = Run(dictOfInterest)
+    assert expectedRun == run
 
 
 def generate_summary_activity(type, commuteFlag):
@@ -37,4 +54,36 @@ def generate_summary_activity(type, commuteFlag):
         "start_latitude": [random_utils.randomCord()[0]],
         "start_longitude": [random_utils.randomCord()[1]],
         "achievement_count": random_utils.randint(0, 10)
+    }
+
+
+def generate_detailed_activity():
+    return {
+        "id": random_utils.randint(0, 10000),
+        "resource_state": random_utils.randint(0, 10000),
+        "athlete": {
+            "id": random_utils.randint(0, 10000),
+            "resource_state": random_utils.randint(0, 10000)
+        },
+        "name": random_utils.randomString(6),
+        "distance": random_utils.randint(0, 10000),
+        "moving_time": random_utils.randint(0, 10000),
+        "elapsed_time": random_utils.randint(0, 10000),
+        "type": "Ride",
+        "start_latlng": random_utils.randomCord(),
+        "end_latlng": random_utils.randomCord(),
+        "start_latitude": random_utils.randomCord()[0],
+        "start_longitude": random_utils.randomCord()[1],
+        "achievement_count": random_utils.randint(0, 10000),
+        "kudos_count": random_utils.randint(0, 10000),
+        "comment_count": random_utils.randint(0, 10000),
+        "map": {
+            "id": random_utils.randomString(6),
+            "polyline": random_utils.randomString(20),
+            "resource_state": random_utils.randint(0, 10000),
+            "summary_polyline": random_utils.randomString(6)
+        },
+        "trainer": False,
+        "commute": False,
+        "manual": False,
     }
