2cc7f5bb045867b2479c839b43b63c6803fbb9a9
Nick <ngonzo95@gmail.com>
2019-11-21 19:23:48 -0600

    enabled adding a last_update param to the url

--- app/main/service/update_user_service.py
+++ app/main/service/update_user_service.py
@@ -5,16 +5,19 @@ import app.main.service.run_service as run_service
 
 MAX_RUNS_TO_COLLECT = 60
 
+
 def updateUser(userId):
     userAuth = user_auth_service.getUserAuthById(userId)
-    collectNewRuns(userAuth)
+    runs = collectNewRuns(userAuth)
+    for run in runs:
+        run_service.createNewRun(runs)
 
 
-
-def collectNewRuns(userAuth):
+def collectNewRuns(userAuth, last_update=0):
     # Loop through all of the pages until we run out of pages
     pageNum = 1
-    activities = strava_backend.list_activities(userAuth, pageNum)
+    activities = strava_backend.list_activities(userAuth, pageNum,
+                                                lastUpdate=last_update)
     ids = interpreter.extract_ids_of_interest_from_activity_list(activities)
 
     while len(activities) == strava_backend.ACTIVITIES_PER_PAGE:
@@ -26,7 +29,11 @@ def collectNewRuns(userAuth):
         ids += interpreter \
             .extract_ids_of_interest_from_activity_list(activities)
 
+    runs = []
     for id in ids:
         activity = strava_backend.get_activity_by_id(userAuth, id)
-        run = interpreter.create_run_from_detailed_activity(userId, activity)
-        run_service.createNewRun(run)
+        run = interpreter.create_run_from_detailed_activity(userAuth.id,
+                                                            activity)
+        runs.append(run)
+
+    return runs
--- app/tests/unit/service/update_user_service_test.py
+++ app/tests/unit/service/update_user_service_test.py
@@ -1,109 +1,37 @@
-from flask import current_app
-import pytest
-from app.tests.helpers.test_app_builder import buildTestApp
-from app.main import dynamo
-from botocore.exceptions import ClientError
 import app.main.service.update_user_service as unit
-import app.main.service.user_auth_service as user_auth_service
 from app.tests.helpers.builder.user_auth_builder import buildUserAuth
 import app.tests.helpers.util.mock_strava_responses as strava_api
 from app.tests.helpers.builder.run_builder import buildRun
 import app.tests.helpers.util.random_utils as random_utils
 import requests_mock
-import app.main.service.run_service as run_service
-from boto3.dynamodb.conditions import Key
-
-userAuthsForDbToDelete = []
-runsForDbToDelete = []
 
 
-def test_update_user_inserts_runs_into_db(test_client):
-    userAuth = generateUserAuth()
+def test_collect_runs_returns_the_runs_of_interest():
+    userAuth = buildUserAuth()
     runIds = []
     for i in range(10):
         id = random_utils.randint(0, 10000)
         run = buildRun(overridenValues={'id': str(id), 'userId': userAuth.id})
-        runsForDbToDelete.append(run)
         runIds.append(id)
 
     with requests_mock.Mocker() as m:
         strava_api.generate_mock_strava_api(runIds, 30, m)
-        unit.updateUser(userAuth.id)
+        runs = unit.collectNewRuns(userAuth, last_update=5)
 
-    dbResponse = run_service.runTable().query(
-        KeyConditionExpression=Key('userId').eq(userAuth.id)
-    )
-
-    assert len(dbResponse['Items']) == 10
-
-# Test for page stoping but commented out to decrease load on db
-# def test_update_user_only_inserts_runs_up_to_max_into_db(test_client):
-#     userAuth = generateUserAuth()
-#     runIds = []
-#     for i in range(40):
-#         id = random_utils.randint(0, 10000)
-#         run = buildRun(overridenValues={'id': str(id), 'userId': userAuth.id})
-#         runsForDbToDelete.append(run)
-#         runIds.append(id)
-#
-#     with requests_mock.Mocker() as m:
-#         strava_api.generate_mock_strava_api(runIds, 0, m)
-#         unit.MAX_RUNS_TO_COLLECT = 10
-#         unit.updateUser(userAuth.id)
-#
-#     dbResponse = run_service.runTable().query(
-#         KeyConditionExpression=Key('userId').eq(userAuth.id)
-#     )
-#
-#     assert len(dbResponse['Items']) == 30
+    assert len(runs) == 10
 
 
-def generateUserAuth():
+def test_update_user_only_inserts_runs_up_to_max_into_db():
     userAuth = buildUserAuth()
-    userAuthsForDbToDelete.append(userAuth)
-    user_auth_service.createUserAuth(userAuth)
-    return userAuth
+    runIds = []
+    for i in range(40):
+        id = random_utils.randint(0, 10000)
+        run = buildRun(overridenValues={'id': str(id), 'userId': userAuth.id})
+        runIds.append(id)
 
+    with requests_mock.Mocker() as m:
+        strava_api.generate_mock_strava_api(runIds, 0, m)
+        unit.MAX_RUNS_TO_COLLECT = 10
+        runs = unit.collectNewRuns(userAuth)
 
-@pytest.fixture(scope='module')
-def test_client():
-    app = buildTestApp()
-
-    # Establish an application context before running the tests.
-    ctx = app.app_context()
-    ctx.push()
-    yield app.test_client()  # this is where the testing happens!
-
-    # After
-    unit.MAX_RUNS_TO_COLLECT = 30
-    exceptionsToRaise = []
-    cleanupUserAuth(exceptionsToRaise)
-    cleanUpRuns(exceptionsToRaise)
-
-    for e in exceptionsToRaise:
-        print(e.response['Error']['Message'])
-
-    if not exceptionsToRaise == []:
-        raise Exception("Error while trying to delete items from db")
-
-    ctx.pop()
-
-
-def cleanupUserAuth(exceptions):
-    tableName = current_app.config['TABLE_NAMES']['USER_AUTH_TABLE']
-    for userAuth in userAuthsForDbToDelete:
-        try:
-            dynamo.get_table(tableName).delete_item(
-                Key={'id': userAuth.id})
-        except ClientError as e:
-            exceptions.append(e)
-
-
-def cleanUpRuns(exceptions):
-    tableName = current_app.config['TABLE_NAMES']['RUN_TABLE']
-    for run in runsForDbToDelete:
-        try:
-            dynamo.get_table(tableName).delete_item(
-                Key={'id': run.id, 'userId': run.userId})
-        except ClientError as e:
-            exceptions.append(e)
+    assert len(runs) == 30
