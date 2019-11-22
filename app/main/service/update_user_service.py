bc3795c950bdf43553bfdbd474baa62367869dfa
Nick <ngonzo95@gmail.com>
2019-11-22 11:04:01 -0600

    Added a service method for updating and creating run maps

--- app/main/service/update_user_service.py
+++ app/main/service/update_user_service.py
@@ -2,6 +2,9 @@ import app.main.service.interpret_strava_response_service as interpreter
 import app.main.service.strava_backend_service as strava_backend
 import app.main.service.user_auth_service as user_auth_service
 import app.main.service.run_service as run_service
+import app.main.service.geo_service as geo_service
+from app.main.model.run_map import RunMap
+import random
 
 MAX_RUNS_TO_COLLECT = 60
 
@@ -37,3 +40,49 @@ def collectNewRuns(userAuth, last_update=0):
         runs.append(run)
 
     return runs
+
+
+def addRunsToRunMaps(runMaps, runs):
+    # Map of new runs to add
+    newRuns = {}
+    for runMap in runMaps:
+        newRuns[runMap.id] = []
+
+    newRunMaps = []
+    for run in runs:
+        runAdded = False
+        for runMap in runMaps:
+            if geo_service.is_point_in_zone(run.start, runMap.center):
+                newRuns[runMap.id].append(run.id)
+                runAdded = True
+                break
+        for runMap in newRunMaps:
+            if geo_service.is_point_in_zone(run.start, runMap.center):
+                runMap.runs.append(run.id)
+                runAdded = True
+                break
+        if not runAdded:
+            map = createRunMapFromRun(run)
+            newRunMaps.append(map)
+
+    # append the runs to each run map
+    for runMap in runMaps:
+        runsToAdd = newRuns[runMap.id]
+        if not runsToAdd == []:
+            runMap.runs = runsToAdd + runMap.runs
+
+    return runMaps, newRunMaps
+
+
+def createRunMapFromRun(run):
+    city = geo_service.get_city_name(run.start)
+
+    values = {
+        'center': run.start,
+        'zoom': 7,
+        'userId': run.userId,
+        'runs': [run.id],
+        'id': city + str(random.randint(0, 1000)),
+        'mapName': city
+    }
+    return RunMap(values)
--- app/tests/unit/service/update_user_service_test.py
+++ app/tests/unit/service/update_user_service_test.py
@@ -2,8 +2,10 @@ import app.main.service.update_user_service as unit
 from app.tests.helpers.builder.user_auth_builder import buildUserAuth
 import app.tests.helpers.util.mock_strava_responses as strava_api
 from app.tests.helpers.builder.run_builder import buildRun
+from app.tests.helpers.builder.run_map_builder import buildRunMap
 import app.tests.helpers.util.random_utils as random_utils
 import requests_mock
+from decimal import Decimal
 
 
 def test_collect_runs_returns_the_runs_of_interest():
@@ -35,3 +37,56 @@ def test_update_user_only_inserts_runs_up_to_max_into_db():
         runs = unit.collectNewRuns(userAuth)
 
     assert len(runs) == 30
+
+
+def test_create_run_map_from_run_sets_name_needed_values():
+    run = buildRun(overridenValues={
+                   'start': [Decimal(44.946636), Decimal(-93.293241)]})
+    runMap = unit.createRunMapFromRun(run)
+
+    assert runMap == buildRunMap(overridenValues={'mapName': 'Minneapolis',
+                                                  'runs': [run.id],
+                                                  'center': run.start,
+                                                  'zoom': 7,
+                                                  'userId': run.userId,
+                                                  'id': runMap.id})
+
+
+def test_add_runs_to_run_maps():
+    #Set up a few run maps with given locations
+    startLocations = [[Decimal(75.3425), Decimal(-95.300)],
+                      [Decimal(44.946636), Decimal(-93.293241)],
+                      [Decimal(54.8675), Decimal(-85.123)],
+                      ]
+    runMaps = []
+    expectedRunList = []
+    runs = []
+
+    for start in startLocations:
+        runMap = buildRunMap(overridenValues={'center': start})
+        expectedRunList.append(runMap.runs.copy())
+        runMaps.append(runMap)
+
+    # build a handfull of runs around the start location that must be added to
+    # the map
+    for i in range(20):
+        index = random_utils.randint(0, len(startLocations)-1)
+        run = buildRun(overridenValues={'start': startLocations[index]})
+        expectedRunList[index] = [run.id] + expectedRunList[index]
+        runs.append(run)
+
+    # Now add a few runs that are not apart of any existing run map
+    newLocation = [Decimal(40.688237), Decimal(-112.073817)]
+    runsFromNewMap = [buildRun(overridenValues={'start': newLocation})]
+    runsFromNewMap.append(buildRun(overridenValues={'start': newLocation}))
+    runs += runsFromNewMap
+
+    #reverse them so the 'newest ones' are first
+    runs = reversed(runs)
+    updatedMaps, newMaps = unit.addRunsToRunMaps(runMaps, runs)
+
+    for i in range(len(runMaps)):
+        assert updatedMaps[i].runs == expectedRunList[i]
+
+    assert newMaps[0].runs == [runsFromNewMap[1].id, runsFromNewMap[0].id]
+    assert newMaps[0].center == newLocation
